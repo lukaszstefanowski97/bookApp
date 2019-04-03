2544089b2e556bca71f28e0e852b70cf94be176e
lukasz.stefanowski97@gmail.com <kOpRoFaGiA97>
2019-04-04 00:22:50 +0200

    response message

--- src/main/java/com/example/bookApp/config/Constants.java
+++ src/main/java/com/example/bookApp/config/Constants.java
@@ -3,8 +3,8 @@ package com.example.bookApp.config;
 public class Constants {
 
     public static final String INVALID_AUTHOR_MESSAGE = "Invalid author input. Forename and surename should start " +
-            "with \"A\".";
-    public static final String INVALID_ISBN_MESSAGE = "ISBN number should be given as string with \" +\n" +
-            "                    \"length of 13 digits and it should be integer.";
+            "with 'A'.";
+    public static final String INVALID_ISBN_MESSAGE = "ISBN number should be given as string with length of 13 " +
+            "digits and it should be integer.";
     public static final String INVALID_INPUT_MESSAGE = INVALID_AUTHOR_MESSAGE + " " + INVALID_ISBN_MESSAGE;
 }
