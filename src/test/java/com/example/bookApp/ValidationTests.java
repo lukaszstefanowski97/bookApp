ed0e0795395d3b817451677faf0e21d4f76c6491
lukaszstefanowski97 <lukasz.stefanowski97@gmail.com>
2019-05-27 15:05:25 +0200

    junit

--- src/test/java/com/example/bookApp/ValidationTests.java
+++ src/test/java/com/example/bookApp/ValidationTests.java
@@ -35,4 +35,30 @@ public class ValidationTests {
         assertNotNull(result5);
         assertEquals(false, result5);
     }
+
+    @Test
+    public void isbnShouldBeValid() {
+        InputValidation validation = new InputValidationImpl();
+
+        boolean result1 = validation.validateIsbn("Adam Adam");
+        boolean result2 = validation.validateIsbn("1234567890987");
+        boolean result3 = validation.validateIsbn("12345678909875");
+        boolean result4 = validation.validateIsbn("1234567g90987");
+        boolean result5 = validation.validateIsbn(null);
+
+        assertNotNull(result1);
+        assertEquals(false, result1);
+
+        assertNotNull(result2);
+        assertEquals(true, result2);
+
+        assertNotNull(result3);
+        assertEquals(false, result3);
+
+        assertNotNull(result4);
+        assertEquals(false, result4);
+
+        assertNotNull(result5);
+        assertEquals(false, result5);
+    }
 }
